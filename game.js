3d9a7120bd74776a869213d06827c975e09a5546
winnies26 <winnie.sun.26@gmail.com>
2020-04-27 14:39:54 -0700

    Complete game

--- game.js
+++ game.js
@@ -10,15 +10,56 @@ var level = 0;
 //Start
 var hasStarted = false;
 
-if (!hasStarted){
-   $(document).keydown(function(event){
+
+$(document).keydown(function(event) {
+  if (hasStarted == false) {
     nextSequence();
+    hasStarted = true;
+  }
 })
-hasStarted = true;
-};
+
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  hasStarted = false;
+}
+
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+    if (userClickedPattern.length == gamePattern.length) {
+      console.log("Right!")
+
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+      userClickedPattern = [];
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+    }, 200);
+    startOver();
+  }
+
+}
+
 function nextSequence() {
 
-  $("h1").text("Level "+level);
+  $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -41,10 +82,3 @@ function animatePress(currentColour) {
 };
 
 // nextSequence();
-
-$(".btn").click(function() {
-  var userChosenColour = $(this).attr('id');
-  userClickedPattern.push(userChosenColour);
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-});
